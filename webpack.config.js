5be188ba290deff3f813e5e07014f7b5ab9af6d9
Hiếu Đỗ <hieu.do@ursa.vn>
2021-12-07 16:42:28 +0700

    Optimized webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require('path');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const plugins = [
+    new webpack.ProvidePlugin({
+        process: 'process/browser',
+    }),
+    new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].[contenthash].css",
+        chunkFilename:"[id].[contenthash].css",
+    }),
+    new webpack.HotModuleReplacementPlugin() // for development
+];
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: './src/index.js',
     output: {
-        filename: 'app.js',
+        filename: 'app.min.js',
         path: path.resolve(__dirname, 'dist')
     },
     resolve: {
@@ -13,9 +27,34 @@ module.exports = {
             process: "process/browser"
         },
     },
-    plugins: [
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-        })
-    ]
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                // Thiết lập lưu các ảnh sử dụng bởi CSS
+                // lưu dưới đường dẫn images cùng file site.css
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            // Image sử dụng bởi CSS lưu tại
+                            publicPath: './assets/images',
+                            emitFile: false
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+    plugins: plugins
 };
